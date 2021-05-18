3f559f49781335a88edcc6606925f73ada89f3f4
Gabriel Mateus <gmsf19@gmail.com>
2021-05-18 20:57:38 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps){
   return(
     <Flex align="center" ml="auto">
 
 
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Mateus</Text>
-        <Text color="gray.300" fontSize="small">
-          gmsf19@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Mateus</Text>
+          <Text color="gray.300" fontSize="small">
+            gmsf19@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
